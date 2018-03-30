0c5f3706326b07c5c29b0a4eec9d231334a4231f
Gianni Bragante <gbragante@gmail.com>
2018-03-30 14:29:58 +0200

    Added DSC-Collect
    Added DSC-Collect

---
+++ DSC-Collect/DSC-Collect.ps1
@@ -0,0 +1,226 @@
+$version = "DSC-Collect (20180330)"
+# by Gianni Bragante - gbrag@microsoft.com
+
+Function Write-Log {
+  param( [string] $msg )
+  $msg = (get-date).ToString("yyyyMMdd HH:mm:ss.fff") + " " + $msg
+  Write-Host $msg
+  $msg | Out-File -FilePath $outfile -Append
+}
+
+Function ExecQuery {
+  param(
+    [string] $NameSpace,
+    [string] $Query
+  )
+  Write-Log ("Executing query " + $Query)
+  if ($PSVersionTable.psversion.ToString() -ge "3.0") {
+    $ret = Get-CimInstance -Namespace $NameSpace -Query $Query -ErrorAction Continue 2>>$errfile
+  } else {
+    $ret = Get-WmiObject -Namespace $NameSpace -Query $Query -ErrorAction Continue 2>>$errfile
+  }
+  Write-Log (($ret | measure).count.ToString() + " results")
+  return $ret
+}
+
+Function ArchiveLog {
+  param( [string] $LogName )
+  $cmd = "wevtutil al """+ $resDir + "\" + $env:computername + "-" + $LogName + ".evtx"" /l:en-us >>""" + $outfile + """ 2>>""" + $errfile + """"
+  Write-Log $cmd
+  Invoke-Expression $cmd
+}
+
+$myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal = new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+if (-not $myWindowsPrincipal.IsInRole($adminRole)) {
+  Write-Output "This script needs to be run as Administrator"
+  exit
+}
+
+$Root = Split-Path (Get-Variable MyInvocation).Value.MyCommand.Path
+
+$resName = "DSC-Results-" + $env:computername +"-" + $(get-date -f yyyyMMdd_HHmmss)
+$resDir = $Root + "\" + $resName
+$outfile = $resDir + "\script-output.txt"
+$errfile = $resDir + "\script-errors.txt"
+
+New-Item -itemtype directory -path $resDir | Out-Null
+
+Write-Log $version
+
+Write-Log "PowerShell version"
+$PSVersionTable | Out-File -FilePath ($resDir + "\PSVersion.txt") -Append
+
+if (Test-Path -Path "C:\Program Files\WindowsPowerShell\DscService\RegistrationKeys.txt") {
+  Write-Log "Registration keys"
+  Copy-Item "C:\Program Files\WindowsPowerShell\DscService\RegistrationKeys.txt" ($resDir + "\RegistrationKeys.txt")
+}
+
+if (Test-Path -Path "C:\inetpub\PSDSCPullServer\web.config") {
+  Write-Log "Pull server web.config"
+  Copy-Item "C:\inetpub\PSDSCPullServer\web.config" ($resDir + "\web.config")
+}
+
+if (Test-Path -Path "C:\Program Files\WindowsPowerShell\DscService\Devices.edb") {
+  $cmd = "cmd.exe /c esentutl.exe /y ""C:\Program Files\WindowsPowerShell\DscService\Devices.edb"" /vssrec"
+  Write-Log $cmd
+  Invoke-Expression $cmd
+  Move-Item .\Devices.edb $resDir
+}
+
+Write-Log "DSC Configuration"
+Copy-Item "C:\Windows\System32\Configuration" -Recurse $resDir
+
+Write-Log "DSC Service Configuration"
+New-Item -itemtype directory -path ($resDir + "\DscService") | Out-Null
+Copy-Item "C:\Program Files\WindowsPowerShell\DscService\Configuration" -Recurse ($resDir + "\DscService")
+
+Write-Log "Installed certificates"
+Get-ChildItem Cert:\LocalMachine\My\ | Out-File -FilePath ($resDir + "\CertLocalMachineMy.txt")
+
+Write-Log "Get-Module output"
+Get-Module -ListAvailable | Out-File -FilePath ($resDir + "\Get-Module.txt")
+
+Write-Log "Get-DscResource output"
+Get-DscResource | Out-File -FilePath ($resDir + "\Get-DscResource.txt")
+
+Write-Log "Get-DscLocalConfigurationManager output"
+Get-DscLocalConfigurationManager | Out-File -FilePath ($resDir + "\Get-DscLocalConfigurationManager.txt")
+
+Write-Log "Get-DscConfiguration output"
+Get-DscConfiguration | Out-File -FilePath ($resDir + "\Get-DscConfiguration.txt")
+
+Write-Log "Get-DscConfigurationStatus output"
+Get-DscConfigurationStatus -all | Out-File -FilePath ($resDir + "\Get-DscConfigurationStatus.txt")
+
+Write-Log "Exporting Application log"
+$cmd = "wevtutil epl Application """+ $resDir + "\" + $env:computername + "-Application.evtx"" >>""" + $outfile + """ 2>>""" + $errfile + """"
+Write-Log $cmd
+Invoke-Expression $cmd
+ArchiveLog "Application"
+
+Write-Log "Exporting System log"
+$cmd = "wevtutil epl System """+ $resDir + "\" + $env:computername + "-System.evtx"" >>""" + $outfile + """ 2>>""" + $errfile + """"
+Write-Log $cmd
+Invoke-Expression $cmd
+ArchiveLog "System"
+
+Write-Log "Exporting WMI-Activity/Operational log"
+$cmd = "wevtutil epl Microsoft-Windows-WMI-Activity/Operational """+ $resDir + "\" + $env:computername + "-WMI-Activity.evtx"" >>""" + $outfile + """ 2>>""" + $errfile + """"
+Write-Log $cmd
+Invoke-Expression $cmd
+ArchiveLog "WMI-Activity"
+
+Write-Log "Exporting DSC log"
+$cmd = "wevtutil epl Microsoft-Windows-DSC/Operational """+ $resDir + "\" + $env:computername + "-DSC.evtx"" >>""" + $outfile + """ 2>>""" + $errfile + """"
+Write-Log $cmd
+Invoke-Expression $cmd
+ArchiveLog "DSC"
+
+Write-Log "Exporting DSC PullServer log"
+$cmd = "wevtutil epl Microsoft-Windows-Powershell-DesiredStateConfiguration-PullServer/Operational """+ $resDir + "\" + $env:computername + "-PullServer.evtx"" >>""" + $outfile + """ 2>>""" + $errfile + """"
+Write-Log $cmd
+Invoke-Expression $cmd
+ArchiveLog "PullServer"
+
+Write-Log "Exporting DSC FileDownloadManager log"
+$cmd = "wevtutil epl Microsoft-Windows-PowerShell-DesiredStateConfiguration-FileDownloadManager/Operational """+ $resDir + "\" + $env:computername + "-FileDownloadManager.evtx"" >>""" + $outfile + """ 2>>""" + $errfile + """"
+Write-Log $cmd
+Invoke-Expression $cmd
+ArchiveLog "FileDownloadManager"
+
+Write-Log "Exporting PowerShell log"
+$cmd = "wevtutil epl Microsoft-Windows-PowerShell/Operational """+ $resDir + "\" + $env:computername + "-PowerShell.evtx""" + $RdrOut + $RdrErr
+Write-Log $cmd
+Invoke-Expression $cmd
+ArchiveLog "PowerShell"
+
+Write-Log "Exporting Windows Remote Management log"
+$cmd = "wevtutil epl Microsoft-Windows-WinRM/Operational """+ $resDir + "\" + $env:computername + "-WindowsRemoteManagement.evtx""" + $RdrOut + $RdrErr
+Write-Log $cmd
+Invoke-Expression $cmd
+ArchiveLog "WindowsRemoteManagement"
+
+Write-Log "Collecting details about running processes"
+$proc = ExecQuery -Namespace "root\cimv2" -Query "select Name, CreationDate, ProcessId, ParentProcessId, WorkingSetSize, UserModeTime, KernelModeTime, ThreadCount, HandleCount, CommandLine from Win32_Process"
+if ($PSVersionTable.psversion.ToString() -ge "3.0") {
+  $StartTime= @{e={$_.CreationDate.ToString("yyyyMMdd HH:mm:ss")};n="Start time"}
+} else {
+  $StartTime= @{n='StartTime';e={$_.ConvertToDateTime($_.CreationDate)}}
+}
+
+if ($proc) {
+  $proc | Sort-Object Name |
+  Format-Table -AutoSize -property @{e={$_.ProcessId};Label="PID"}, @{e={$_.ParentProcessId};n="Parent"}, Name,
+  @{N="WorkingSet";E={"{0:N0}" -f ($_.WorkingSetSize/1kb)};a="right"},
+  @{e={[DateTime]::FromFileTimeUtc($_.UserModeTime).ToString("HH:mm:ss")};n="UserTime"}, @{e={[DateTime]::FromFileTimeUtc($_.KernelModeTime).ToString("HH:mm:ss")};n="KernelTime"},
+  @{N="Threads";E={$_.ThreadCount}}, @{N="Handles";E={($_.HandleCount)}}, $StartTime, CommandLine |
+  Out-String -Width 500 | Out-File -FilePath ($resDir + "\processes.txt")
+
+  Write-Log "Collecting services details"
+  $svc = ExecQuery -NameSpace "root\cimv2" -Query "select  ProcessId, DisplayName, StartMode,State, Name, PathName, StartName from Win32_Service"
+
+  if ($svc) {
+    $svc | Sort-Object DisplayName | Format-Table -AutoSize -Property ProcessId, DisplayName, StartMode,State, Name, PathName, StartName |
+    Out-String -Width 400 | Out-File -FilePath ($resDir + "\services.txt")
+  }
+
+  Write-Log "Collecting system information"
+  $pad = 27
+  $OS = ExecQuery -Namespace "root\cimv2" -Query "select Caption, CSName, OSArchitecture, BuildNumber, InstallDate, LastBootUpTime, LocalDateTime, TotalVisibleMemorySize, FreePhysicalMemory, SizeStoredInPagingFiles, FreeSpaceInPagingFiles from Win32_OperatingSystem"
+  $CS = ExecQuery -Namespace "root\cimv2" -Query "select Model, Manufacturer, SystemType, NumberOfProcessors, NumberOfLogicalProcessors, TotalPhysicalMemory, DNSHostName, Domain, DomainRole from Win32_ComputerSystem"
+  $BIOS = ExecQuery -Namespace "root\cimv2" -query "select BIOSVersion, Manufacturer, ReleaseDate, SMBIOSBIOSVersion from Win32_BIOS"
+  $TZ = ExecQuery -Namespace "root\cimv2" -Query "select Description from Win32_TimeZone"
+  $PR = ExecQuery -Namespace "root\cimv2" -Query "select Name, Caption from Win32_Processor"
+
+  "Computer name".PadRight($pad) + " : " + $OS.CSName | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Model".PadRight($pad) + " : " + $CS.Model | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Manufacturer".PadRight($pad) + " : " + $CS.Manufacturer | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "BIOS Version".PadRight($pad) + " : " + $BIOS.BIOSVersion | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "BIOS Manufacturer".PadRight($pad) + " : " + $BIOS.Manufacturer | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "BIOS Release date".PadRight($pad) + " : " + $BIOS.ReleaseDate | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "SMBIOS Version".PadRight($pad) + " : " + $BIOS.SMBIOSBIOSVersion | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "SystemType".PadRight($pad) + " : " + $CS.SystemType | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Processor".PadRight($pad) + " : " + $PR.Name + " / " + $PR.Caption | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Processors physical/logical".PadRight($pad) + " : " + $CS.NumberOfProcessors + " / " + $CS.NumberOfLogicalProcessors | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Memory physical/visible".PadRight($pad) + " : " + ("{0:N0}" -f ($CS.TotalPhysicalMemory/1mb)) + " MB / " + ("{0:N0}" -f ($OS.TotalVisibleMemorySize/1kb)) + " MB" | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Free physical memory".PadRight($pad) + " : " + ("{0:N0}" -f ($OS.FreePhysicalMemory/1kb)) + " MB" | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Paging files size / free".PadRight($pad) + " : " + ("{0:N0}" -f ($OS.SizeStoredInPagingFiles/1kb)) + " MB / " + ("{0:N0}" -f ($OS.FreeSpaceInPagingFiles/1kb)) + " MB" | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Operating System".PadRight($pad) + " : " + $OS.Caption + " " + $OS.OSArchitecture | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Build Number".PadRight($pad) + " : " + $OS.BuildNumber | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Time zone".PadRight($pad) + " : " + $TZ.Description | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Install date".PadRight($pad) + " : " + $OS.InstallDate | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Last boot time".PadRight($pad) + " : " + $OS.LastBootUpTime | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Local time".PadRight($pad) + " : " + $OS.LocalDateTime | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "DNS Hostname".PadRight($pad) + " : " + $CS.DNSHostName | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  "Domain".PadRight($pad) + " : " + $CS.Domain | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+  $roles = "Standalone Workstation", "Member Workstation", "Standalone Server", "Member Server", "Backup Domain Controller", "Primary Domain Controller"
+  "Domain role".PadRight($pad) + " : " + $roles[$CS.DomainRole] | Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+
+  $drives = @()
+  $drvtype = "Unknown", "No Root Directory", "Removable Disk", "Local Disk", "Network Drive", "Compact Disc", "RAM Disk"
+  $Vol = ExecQuery -NameSpace "root\cimv2" -Query "select * from Win32_LogicalDisk"
+  foreach ($disk in $vol) {
+    $drv = New-Object PSCustomObject
+    $drv | Add-Member -type NoteProperty -name Letter -value $disk.DeviceID 
+    $drv | Add-Member -type NoteProperty -name DriveType -value $drvtype[$disk.DriveType]
+    $drv | Add-Member -type NoteProperty -name VolumeName -value $disk.VolumeName 
+    $drv | Add-Member -type NoteProperty -Name TotalMB -Value ($disk.size)
+    $drv | Add-Member -type NoteProperty -Name FreeMB -value ($disk.FreeSpace)
+    $drives += $drv
+  }
+  $drives | 
+  Format-Table -AutoSize -property Letter, DriveType, VolumeName, @{N="TotalMB";E={"{0:N0}" -f ($_.TotalMB/1MB)};a="right"}, @{N="FreeMB";E={"{0:N0}" -f ($_.FreeMB/1MB)};a="right"} |
+  Out-File -FilePath ($resDir + "\SystemInfo.txt") -Append
+} else {
+  $proc = Get-Process | Where-Object {$_.Name -ne "Idle"}
+  $proc | Format-Table -AutoSize -property id, name, @{N="WorkingSet";E={"{0:N0}" -f ($_.workingset/1kb)};a="right"},
+  @{N="VM Size";E={"{0:N0}" -f ($_.VirtualMemorySize/1kb)};a="right"},
+  @{N="Proc time";E={($_.TotalProcessorTime.ToString().substring(0,8))}}, @{N="Threads";E={$_.threads.count}},
+  @{N="Handles";E={($_.HandleCount)}}, StartTime, Path | 
+  Out-String -Width 300 | Out-File -FilePath ($resDir + "\processes.txt")
+}
+
+Write-Log "Collecting the list of installed hotfixes"
+Get-HotFix -ErrorAction SilentlyContinue 2>>$errfile | Sort-Object -Property InstalledOn | Out-File $resDir\hotfixes.txt
