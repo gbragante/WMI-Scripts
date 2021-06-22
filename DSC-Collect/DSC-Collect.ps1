a17afcc0a7f46c2bd886344c2e9ab8c7d6d8fd3f
gbrag@microsoft.com <gbrag@microsoft.com>
2021-06-22 12:18:09 +0200

    Added EULA
    Added EULA

--- DSC-Collect/DSC-Collect.ps1
+++ DSC-Collect/DSC-Collect.ps1
@@ -1,4 +1,6 @@
-$version = "DSC-Collect (20210531)"
+param( [string]$Path, [switch]$AcceptEula )
+
+$version = "DSC-Collect (20210622)"
 # by Gianni Bragante - gbrag@microsoft.com
 
 $myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -8,14 +10,6 @@ if (-not $myWindowsPrincipal.IsInRole($adminRole)) {
   Write-Output "This script needs to be run as Administrator"
   exit
 }
-
-Write-Host "This script is designed to collect information that will help Microsoft Customer Support Services (CSS) troubleshoot an issue you may be experiencing with Windows."
-Write-Host "The collected data may contain Personally Identifiable Information (PII) and/or sensitive data, such as (but not limited to) IP addresses, PC names, and user names."
-Write-Host "Once the tracing and data collection has completed, the script will save the data in a subfolder. This folder is not automatically sent to Microsoft."
-Write-Host "You can send this folder to Microsoft CSS using a secure file transfer tool - Please discuss this with your support professional and also any concerns you may have."
-Write-Host "Find our privacy statement here: https://privacy.microsoft.com/en-us/privacy"
-$confirm = Read-Host ("Are you sure you want to continue[Y/N]?")
-if ($confirm.ToLower() -ne "y") {exit}
 
 $global:Root = Split-Path (Get-Variable MyInvocation).Value.MyCommand.Path
 
@@ -29,6 +23,18 @@ Import-Module ($global:Root + "\Collect-Commons.psm1") -Force
 New-Item -itemtype directory -path $global:resDir | Out-Null
 
 Write-Log $version
+if ($AcceptEula) {
+  Write-Log "AcceptEula switch specified, silently continuing"
+  $eulaAccepted = ShowEULAIfNeeded "DSC-Collect" 2
+} else {
+  $eulaAccepted = ShowEULAIfNeeded "DSC-Collect" 0
+  if($eulaAccepted -ne "Yes")
+   {
+     Write-Log "EULA declined, exiting"
+     exit
+   }
+ }
+Write-Log "EULA accepted, continuing"
 
 Write-Log "PowerShell version"
 $PSVersionTable | Out-File -FilePath ($global:resDir + "\PSVersion.txt") -Append
